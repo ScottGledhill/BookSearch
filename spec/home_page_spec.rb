66cb91515916ba99910b4a1c1134c2cc0ffed628
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-09-19 17:49:09 +0100

    launchy gem added

--- spec/home_page_spec.rb
+++ spec/home_page_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'Users' do
   scenario 'can find the home page' do
     visit 'users#index'
-    expect(page).to have_content('Welcome')
-    expect(page).to have_content('log in')
+    expect(page).to have_content('You need to sign in')
   end
 end
--- spec/users/log_in_spec.rb
+++ spec/users/log_in_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature 'Users' do
   scenario 'can log in' do
     visit 'users#index'
-    click_link('log in')
+    save_and_open_page
+    click_link('sign in')
     expect(page).to have_content('username')
   end
 end
