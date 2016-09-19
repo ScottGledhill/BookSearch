d2d910e9d5cfa8a3d91cbaec735e0af169df3d31
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-09-19 17:54:27 +0100

    sign up test passed

--- spec/users/log_in_spec.rb
+++ spec/users/log_in_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 feature 'Users' do
-  scenario 'can log in' do
+  scenario 'can sign up' do
     visit 'users#index'
+    click_link('Sign up')
+    fill_in('Email', :with => '1@2.3.com')
+    fill_in('Password', :with => '111111')
+    fill_in('Password confirmation', :with => '111111')
     save_and_open_page
-    click_link('sign in')
-    expect(page).to have_content('username')
+    click_button('Sign up')
+    expect(page).to have_content('You have signed up successfully')
   end
 end
