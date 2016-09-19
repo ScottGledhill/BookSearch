84e9d1c89912181987296e9d94c21f17a78ee164
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-09-19 18:33:14 +0100

    test added for password confirmation

--- spec/users/sign_up_spec.rb
+++ spec/users/sign_up_spec.rb
@@ -11,7 +11,7 @@ feature 'Users' do
     expect(page).to have_content('You have signed up successfully')
   end
 
-    scenario 'can not sign up with same username' do
+  scenario 'can not sign up with same username' do
     visit 'users#index'
     click_link('Sign up')
     fill_in('Email', :with => '1@2.3.com')
@@ -19,5 +19,15 @@ feature 'Users' do
     fill_in('Password confirmation', :with => '111111')
     click_button('Sign up')
     expect(page).to have_content('Email has already been taken')
+  end
+
+  scenario 'can not sign up with password/password confirmation different' do
+    visit 'users#index'
+    click_link('Sign up')
+    fill_in('Email', :with => '1@222.3.com')
+    fill_in('Password', :with => '111111')
+    fill_in('Password confirmation', :with => '222222')
+    click_button('Sign up')
+    expect(page).to have_content(' Password confirmation doesn\'t match Password')
   end
 end
