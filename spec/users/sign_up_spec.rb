55a5321ae0b1c5852db163fdfe327f5641e06480
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-09-19 18:30:34 +0100

    test added to prevent double sign up wuth same email

--- spec/users/sign_up_spec.rb
+++ spec/users/sign_up_spec.rb
@@ -7,8 +7,17 @@ feature 'Users' do
     fill_in('Email', :with => '1@2.3.com')
     fill_in('Password', :with => '111111')
     fill_in('Password confirmation', :with => '111111')
-    save_and_open_page
     click_button('Sign up')
     expect(page).to have_content('You have signed up successfully')
+  end
+
+    scenario 'can not sign up with same username' do
+    visit 'users#index'
+    click_link('Sign up')
+    fill_in('Email', :with => '1@2.3.com')
+    fill_in('Password', :with => '111111')
+    fill_in('Password confirmation', :with => '111111')
+    click_button('Sign up')
+    expect(page).to have_content('Email has already been taken')
   end
 end
