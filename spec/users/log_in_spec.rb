b4ba0cd6edd448bd7193e0eb5a67f61293df63c3
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-09-20 14:34:24 +0100

    fixed log in test

--- spec/users/log_in_spec.rb
+++ spec/users/log_in_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 feature 'Users' do
   scenario 'can log in' do
     sign_up_new_account
-    visit 'users#sign_in'
+    save_and_open_page
+    click_link('Logout')
     fill_in('Email', :with => '1@22221.3.com')
     fill_in('Password', :with => '111111')
     click_button('Log in')
-    expect(page).to have_content('LOGGED IN')
+    expect(page).to have_content('Signed in successfully')
   end
 end
