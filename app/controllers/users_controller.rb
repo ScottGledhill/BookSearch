478ab6f2a86ce099fe3ace707fd21f42600da824
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-09-21 17:34:09 +0100

    added logged in view, to display search results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
   def index
     if params.include?(:q)
       api_search
+      render :logged_in
+    else
       render :index
     end
   end
 
   def api_search
-    api = ApiSearch.new
-    api.call(params)
+    @api = ApiSearch.new
+    @api.call(params)
   end
 end
--- app/models/api_search.rb
+++ app/models/api_search.rb
@@ -2,6 +2,10 @@ class ApiSearch < ApplicationRecord
 
   def call(params)
      @params = params[:q]
-    p RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{@params}&key=#{ENV['api_key']}")
+     search_result
+  end
+
+  def search_result
+    @search_result = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{@params}&key=#{ENV['api_key']}")
   end
 end
